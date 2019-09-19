3ad4087be567989b8ed411e41007aeaabd6cdc03
Randall Floyd <rdfloyd@indiana.edu>
2019-09-19 09:56:41 -0400

    Fixing call to :id parameter

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ var replaceStream = require('replacestream')
   , contentRequest = require('request');
 
 app.get('/secure-content/:id', function (req, res) {
-  var secureContent = contentRequest('http://purl.dlib.indiana.edu/' + id)
+  console.log('Sending http://purl.dlib.indiana.edu/' + req.params.id)
+  var secureContent = contentRequest('http://purl.dlib.indiana.edu/' + req.params.id)
      .pipe(replaceStream('http://purl', 'https://purl')).pipe(res);
    // res.send(secureContent);
 })
